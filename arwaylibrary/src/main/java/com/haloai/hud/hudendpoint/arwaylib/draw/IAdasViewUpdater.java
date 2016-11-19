ebb5792d6659f819f4e9315f416b7feda373ce7a
wangshengxing <1048381213@qq.com>
2016-11-19 17:01:00 +0800

    修复场景反转问题

---
+++ arwaylibrary/src/main/java/com/haloai/hud/hudendpoint/arwaylib/draw/IAdasViewUpdater.java
@@ -0,0 +1,13 @@
+package com.haloai.hud.hudendpoint.arwaylib.draw;
+
+/**
+ * Created by wangshengxing on 19/11/2016.
+ */
+
+public interface IAdasViewUpdater {
+    /**
+     * 行人检测接口
+     */
+    void showPedestrains();
+    void hidePedestrains();
+}
